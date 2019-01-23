c3c6ca346f2099ef4c30a9a4c0be9a8e91a5e0f5
Xinchen Yan <xcyan@umich.edu>
2019-01-22 19:56:20 -0800

    Update h36m_analogy.py

--- h36m_analogy.py
+++ h36m_analogy.py
@@ -5,7 +5,6 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow import app
-#from H36M_VAEAnalogyModel import VAEAnalogyModel
 from H36M_MTVAEAnalogyModel import MTVAEAnalogyModel
 
 import h36m_input as input_generator
@@ -20,25 +19,25 @@ slim = tf.contrib.slim
 
 flags.DEFINE_string('inp_dir', '/mnt/brain6/scratch/xcyan/experimental/gif2gif/workspace/', '')
 flags.DEFINE_string('dataset_name', 'Human3.6M', '')
-flags.DEFINE_integer('keypoint_dim', 32, '')
-flags.DEFINE_integer('img_size', 180, '')
+flags.DEFINE_string('model_version', 'MTVAE', '')
 flags.DEFINE_string('checkpoint_dir', 'checkpoints/', '')
-flags.DEFINE_integer('batch_size', 1, '')
-#
+flags.DEFINE_string('master', '', '')
 flags.DEFINE_integer('min_input_length', 16, '')
 flags.DEFINE_integer('max_input_length', 16, '')
 flags.DEFINE_integer('max_length', 32, '')
-flags.DEFINE_string('master', '', '')
+# Model paramters.
+flags.DEFINE_integer('keypoint_dim', 32, '')
+flags.DEFINE_integer('img_size', 180, '')
+flags.DEFINE_integer('batch_size', 1, '')
 flags.DEFINE_string('enc_model', 'ln_lstm', '')
 flags.DEFINE_string('dec_model', 'ln_lstm', '')
 flags.DEFINE_integer('use_bidirection_lstm', 0, '')
 flags.DEFINE_integer('use_recurrent_dropout', 0, '')
 flags.DEFINE_float('recurrent_dropout_prob', 1.0, '')
-#
+# Sample parameters.
 flags.DEFINE_integer('use_prior', 0, '')
 flags.DEFINE_integer('sample_pdf', 1, '')
 flags.DEFINE_float('sample_temp', 0.1, '')
-flags.DEFINE_string('model_version', 'MTVAE', '')
 
 FLAGS = flags.FLAGS
 
@@ -118,13 +117,13 @@ def save_html_page(img_dir, quantity):
       sub_name = '%02d_%s' % (t, nick_name)
       content += '\n<tr><td>%s Sequence A (input)</td>' % name
       content += '<td><img src=\"%s_A.png\" style=\"width:300px;height:100px;\"></td></tr>' % sub_name
-      #
+      
       content += '\n<tr><td>%s Sequence B (input)</td>' % name
       content += '<td><img src=\"%s_B.png\" style=\"width:500px;height:100px;\"></td></tr>' % sub_name
-      #
+      
       content += '\n<tr><td>%s Sequence C (input)</td>' % name
       content += '<td><img src=\"%s_C.png\" style=\"width:300px;height:100px;\"></td></tr>' % sub_name
-      #
+      
       content += '\n<tr><td>%s Sequence D (output)</td>' % name
       content += '<td><img src=\"%s_predD.png\" style=\"width:500px;height:100px;\"></td></tr>' % sub_name
     content += '</html>'
@@ -148,27 +147,24 @@ def run_visualization(video_proc_lib, video_dir, video_idx, file_suffix,
                       A_imgs, B_imgs, C_imgs, predD_imgs):
   A_imgs, B_imgs = np.copy(A_imgs), np.copy(B_imgs)
   Cimgs, predD_imgs = np.copy(C_imgs), np.copy(predD_imgs)
-  #########
+
   his_len = A_imgs.shape[0]
   fut_len = B_imgs.shape[0]
-  #
   for t in xrange(his_len):  
     A_imgs[t] = utils.visualize_boundary(A_imgs[t], radius=4, colormap='green')
     C_imgs[t] = utils.visualize_boundary(C_imgs[t], radius=4, colormap='purple')
   for t in xrange(fut_len):
     B_imgs[t] = utils.visualize_boundary(B_imgs[t], radius=4, colormap='blue')
     predD_imgs[t] = utils.visualize_boundary(predD_imgs[t], radius=4, colormap='red')
-  # 
-  #AB_imgs_left = np.concatenate((A_imgs, np.tile(A_imgs[-1:], (fut_len, 1, 1, 1))), axis=0)
+
   AB_imgs_left = np.concatenate((A_imgs, np.zeros_like(B_imgs)), axis=0)
   AB_imgs_right = np.concatenate((A_imgs, B_imgs), axis=0)
   AB_imgs = np.concatenate((AB_imgs_left, AB_imgs_right), axis=2)
-  #
-  #CD_imgs_left = np.concatenate((C_imgs, np.tile(C_imgs[-1:], (fut_len, 1, 1, 1))), axis=0)
+
   CD_imgs_left = np.concatenate((C_imgs, np.zeros_like(predD_imgs)), axis=0)  
   CD_imgs_right = np.concatenate((C_imgs, predD_imgs), axis=0)
   CD_imgs = np.concatenate((CD_imgs_left, CD_imgs_right), axis=2)
-  #
+
   out_dict = dict()
   out_dict['A_imgs'] = np.copy(A_imgs[HIS_KEYFRAMES])
   out_dict['B_imgs'] = np.copy(B_imgs[FUT_KEYFRAMES])
@@ -178,7 +174,7 @@ def run_visualization(video_proc_lib, video_dir, video_idx, file_suffix,
   ## merge videos ##
   ##################
   merged_video = np.concatenate((AB_imgs, CD_imgs), axis=2)
-  #
+
   video_file = '%02d_merged_%s.gif' % (video_idx, file_suffix)
   video_proc_lib.save_img_seq_to_video(merged_video, video_dir, video_file,
     frame_rate=7.5, codec='', override=True)
@@ -203,7 +199,7 @@ def main(_):
   assert params.min_input_length == params.max_input_length
   init_length = params.min_input_length
   sample_length = params.max_length + init_length
-  #
+
   np_data = dict()
   np_data['landmarks'] = np.zeros(
     (dataset_size, sample_length, params.keypoint_dim, 2), dtype=np.float32)
@@ -243,9 +239,8 @@ def main(_):
     keypointClass = MODEL_TO_CLASS[params.model_version]
     model = keypointClass(params)
     scopes = MODEL_TO_SCOPE[params.model_version]
-    #
+
     eval_data = model.get_inputs_from_placeholder(dataset_size, params.batch_size)
-    #print eval_data['AB_landmarks']
     inputs = model.preprocess(eval_data, is_training=False, load_image=False)
     assert (not params.use_prior)
     model_fn = model.get_model_fn(is_training=False, reuse=False)
@@ -262,10 +257,7 @@ def main(_):
     with tf.Session() as sess:
       sess.run(tf.global_variables_initializer())
       restorer.restore(sess, checkpoint_path)
-      #
-      #batch_output = np.zeros(
-      #  (dataset_size, params.max_length, params.keypoint_dim, 2), dtype=np.float32)
-      #
+
       for i in xrange(dataset_size/2):
         print(i)
         AB_idx, CD_idx = i * 2, i * 2 + 1 
@@ -275,9 +267,7 @@ def main(_):
           feed_dict={
             eval_data['AB_landmarks']: np_data['landmarks'][AB_idx:AB_idx+1],
             eval_data['CD_landmarks']: np_data['landmarks'][CD_idx:CD_idx+1]})
-        #
         batch_output = np.copy(predD_lms) * 2 - 1.0  
-        #
         A_imgs = utils.visualize_h36m_skeleton_batch(
           np.copy(A_lms[0]), params.img_size)
         B_imgs = utils.visualize_h36m_skeleton_batch(
