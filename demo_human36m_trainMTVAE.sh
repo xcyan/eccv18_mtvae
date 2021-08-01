e0c79f9b6e49c683883fd8e022cfd513165994bf
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-01 20:23:31 +0800

    Delete demo_human36m_trainMTVAE.sh

--- demo_human36m_trainMTVAE.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python h36m_train_mtvae.py \
-  --inp_dir=workspace/ \
-  --dataset_name=Human3.6M \
-  --checkpoint_dir=checkpoints/ \
-  --model_name=H36M_MTVAE \
-  --max_number_of_steps=60000
-
